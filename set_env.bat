ba01775bc67d8a9448d578345a66500fea44b286
Konstantin Kulikov <rasmadeus@gmail.com>
2019-11-23 19:49:07 +0100

    Make the project's structure

---
+++ set_env.bat
@@ -0,0 +1,3 @@
+set path=%path%;C:\Users\rasmadeus\Documents\dev\sdk\5.13.0\msvc2017_64\bin
+set CMAKE_PREFIX_PATH=C:\Users\rasmadeus\Documents\dev\sdk\5.13.0\msvc2017_64;C:\Users\rasmadeus\Documents\dev\sdk\boost_1_71_0
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
