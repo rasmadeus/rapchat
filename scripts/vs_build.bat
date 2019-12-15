e9565b9d277e4a645a6fdcbc08ccc6bb4b83336a
Konstantin Kulikov <rasmadeus@gmail.com>
2019-12-15 18:02:38 +0100

    Update project structure

--- scripts/vs_build.bat
+++ scripts/vs_build.bat
@@ -2,7 +2,6 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 SET QT_HOME=C:\Users\rasmadeus\Documents\dev\sdk\5.13.0\msvc2017_64
 SET BOOST_HOME=C:\Users\rasmadeus\Documents\dev\sdk\boost_1_71_0
 SET OUT=C:\Users\rasmadeus\Documents\dev\pro\rapchat\build\out
-SET SRC_HOME=C:\Users\rasmadeus\Documents\dev\pro\rapchat\rapchat
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%OUT% -DCMAKE_PREFIX_PATH=%OUT%/find;%QT_HOME%;%BOOST_HOME% %SRC_HOME%
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%OUT% -DCMAKE_PREFIX_PATH=%OUT%/find;%QT_HOME%;%BOOST_HOME% %1
 nmake
 nmake install
