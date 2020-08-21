e440cf867e568da63d5eaa11bedc1e584b819a11
Konstantin Kulikov <rasmadeus@gmail.com>
2020-08-21 20:57:28 +0200

    Add test executable project:     - add the project on purpose to check library

--- libs/rapchat/include/shared_export.h
+++ libs/rapchat/include/shared_export.h
@@ -1,9 +1,9 @@
 #if defined(_WIN32)
-    #if defined(RAPCHAT_LIBRARY)
-        #define RAPCHAT_SHARED_EXPORT __declspec(dllexport)
-    #else
-        #define RAPCHAT_SHARED_EXPORT __declspec(dllimport)
-    #endif
+#if defined(RAPCHAT_LIBRARY)
+#define RAPCHAT_SHARED_EXPORT __declspec(dllexport)
 #else
-    #define RAPCHAT_SHARED_EXPORT
+#define RAPCHAT_SHARED_EXPORT __declspec(dllimport)
+#endif
+#else
+#define RAPCHAT_SHARED_EXPORT
 #endif
---
+++ tests/well_formed/main.cpp
@@ -0,0 +1,9 @@
+#pragma warning(disable: 4251)
+#include <iostream>
+#include <message.h>
+
+int main([[maybe_unused]] char** argv, [[maybe_unused]] int argc)
+{
+    std::cout << "Hello world!";
+    return 0;
+}
