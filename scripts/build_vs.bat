b1b1284c353a898f390bf6a141587be7c25acabd
Konstantin Kulikov <rasmadeus@gmail.com>
2020-08-29 21:26:18 +0200

    Add project for testing new features:     - add project;     - include boost library.

--- scripts/build_vs.bat
+++ scripts/build_vs.bat
@@ -1,6 +1,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-set GTEST_HOME="C:/Users/rasmadeus/Documents/dev/sdk/googletest/out/install/x64-Release"
+set GTEST_ROOT="C:\\Users\\rasmadeus\\Documents\\dev\\sdk\\googletest\\out\\install\\x64-Debug"
+set BOOST_ROOT="C:\\Users\\rasmadeus\\Documents\\dev\\sdk\\boost_1_74_0"
 
-cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH=%GTEST_HOME% -DCMAKE_BUILD_TYPE=Debug %1
+cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH=%GTEST_ROOT%;%BOOST_ROOT% -DCMAKE_BUILD_TYPE=Debug %1
 cmake --build .
